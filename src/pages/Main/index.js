2c6f4d749ad93a136762e5fd4b6e8d119118f37d
jeseias <domingosjeseias@gmail.com>
2020-02-14 14:48:18 +0100

    Clientes basic structure now done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './../../layout/Header';
 import Menu from './../../layout/Menu';
+import Clientes from './../../layout/Clientes';
 
 export default () => {
   return (
     <>
       <Header />
       <Menu />
+      <Clientes />
     </>
   )
 }
