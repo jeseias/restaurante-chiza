bddbc502275a1d29c6d2d88e22fd5fb052877103
jeseias <domingosjeseias@gmail.com>
2020-02-16 23:00:23 +0100

    Worked on the titles of Clientes

--- src/layout/Clientes/styles.js
+++ src/layout/Clientes/styles.js
@@ -1,22 +1,32 @@
 import styled from 'styled-components';
 
-import { light } from './../../styles/variables';
+import { light_brown, dark_brown, yellow_color, light_gold } from './../../styles/variables';
 
 export const Container = styled.section`
   padding: 1rem 5rem;
   position: relative;
   text-align: center;
-  background: ${light};
+  background: ${light_gold};
 
   > div {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 2rem;
+  }
+
+  > h1 {
+    color: ${yellow_color}; 
+
+    &::after, &::before {
+      background: ${light_brown};
+    }
   }
 `;
 
 export const ClientCard = styled.div`
   padding: 1rem;
   text-align: center;
+  background: ${light_brown};
 
   .icon {
     display: block;
