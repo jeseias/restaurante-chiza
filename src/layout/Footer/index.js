3efb41fea1b840ec6eb7ec83139b73ed5c7b5943
jeseias <domingosjeseias@gmail.com>
2020-02-19 17:15:47 +0100

    Done with the footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { FaFacebookSquare, FaInstagram, FaLinkedinIn, FaPlaneDeparture, 
   FaAddressBook, FaTwitterSquare  } from 'react-icons/fa';
-  import { MdEmail, MdPhone, MdLocationOn } from 'react-icons/md';
+  import { MdEmail, MdPhone, MdLocationOn, MdLocationSearching } from 'react-icons/md';
 
 import { Container, Top, Bottom } from './styles';
 
@@ -10,39 +10,39 @@ export default () =>
   <Container>
     <Top>
       <div>
-        <h2>
-          Siga nos
-          <FaPlaneDeparture />
-        </h2>
-        <FaFacebookSquare size={30} />
-        <FaInstagram size={30} />
-        <FaLinkedinIn size={30} />
-        <FaTwitterSquare size={30} />
+        <FaPlaneDeparture size={30} className="mainIcon"/>
+        <h2> Siga nos </h2>
+        <div className="icons">
+          <FaFacebookSquare size={16} className="icon" />
+          <FaInstagram size={16} className="icon"/>
+          <FaLinkedinIn size={16} className="icon" />
+          <FaTwitterSquare size={16} className="icon"/>
+        </div>
       </div>
       <div>
-        <h2>
-          Localização
-          <MdLocationOn />
-        </h2>
-      </div>
-      <div>
-        <h2>
-          Contacto
-          <FaAddressBook />
-        </h2>
-        <span>
-          <MdEmail size={20} className='icon' />
+        <FaAddressBook size={30} className="mainIcon"/>
+        <h2> Contacto </h2>
+        <p>
+          <MdEmail size={17} className='icon' />
           <span>restaurantechiza@outlook.com</span>
-        </span>
-        <span>
-          <MdPhone size={20} className='icon' />
-          <span>912516269/923516269</span>
-        </span>
+        </p>
+        <p>
+          <MdPhone size={17} className='icon' />
+          <span>912516269 / 923516269</span>
+        </p>
+      </div>
+      <div>
+        <MdLocationOn size={30} className="mainIcon"/>
+        <h2> Localização </h2>
+        <p>
+          <MdLocationSearching size={17} />
+          Entrada Do Sequele, Ao lado da ponte.
+        </p>
       </div>
     </Top>
     <Bottom>
-      <p>Feito com Amor por <a href="https://jeseiasdomingos.netlify.com">Jeseías Domingos</a> </p>
+      <p>Feito com Amor por <a href="https://jeseiasdomingos.netlify.com" target="_blank">Jeseías Domingos</a> </p>
+      <br/>
       <p> Copyright &copy; 2020, Restaurante Chiza, Todos Direito Reservados </p>
-      <p>Like A Boss</p>
     </Bottom>
   </Container>
--- src/layout/Footer/styles.js
+++ src/layout/Footer/styles.js
@@ -1,17 +1,87 @@
 import styled from 'styled-components';
 
+import { yellow_color, dark_brown } from './../../styles/variables';
+import { CenterContent } from './../../styles/utils';
+
 export const Container = styled.section`
-  
+  background: ${yellow_color};
 `;
 
 export const Top = styled.div`
-  padding: 2rem;
-  display: flex;
-`;
-
-export const Bottom = styled.div`
+  padding: 3rem;
+  margin: 0 auto;
+  width: 80%;
   display: flex;
   justify-content: space-between;
+  flex-wrap: wrap;
+
+  > div {
+    background: ${dark_brown}; 
+    padding: 2rem;
+    width: 30%;
+    color: #fff;
+    padding: 2rem;
+    padding-top: 5rem;
+    box-shadow: inset 0 0 2rem #000, inset 0 0 1rem ${yellow_color};
+    border: 5px solid #fff;
+    border-top: none;
+
+    &:nth-child(1) {
+      clip-path: polygon(0 0, 100% 18%, 100% 100%, 0% 100%);
+
+      .icons {
+        ${CenterContent};
+
+        svg {
+          position: relative;
+          top: 1.2rem;
+          margin: .6rem;
+        }
+      }
+    }
+
+    &:nth-child(2) {
+      clip-path:  polygon(50% 0, 100% 18%, 100% 100%, 50% 100%, 0 100%, 0 18%);
+
+      p {
+        position: relative;
+        top: .8rem;
+        ${CenterContent};
+      }
+    }
+
+    &:nth-child(3) {
+      clip-path: polygon(0 18%, 100% 0, 100% 100%, 0% 100%);
+      
+      p {
+        position: relative;
+        top: 1.2rem;
+        ${CenterContent};
+      }
+    }
+
+    h2 {
+      font-size: 2.5rem;
+      font-family: 'Dancing Script';
+      text-align: center;
+    }
+
+    .mainIcon {
+      display: block;
+      margin: 1rem auto;
+    }
+
+  }
+`;
+
+export const Bottom = styled.div` 
   color: #fff;
-  background: #333;
+  background: ${dark_brown};
+  padding: 1rem 25rem;
+  text-align: center;
+
+  a {
+    color: #fff;
+    font-weight: bold;
+  }
 `;
