a3931178e95ec806ea1322e4652685e0ab722111
jeseias <domingosjeseias@gmail.com>
2020-02-19 15:42:30 +0100

    alomst done with sobre

--- src/layout/Sobre/index.js
+++ src/layout/Sobre/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-import { Container, SobreBox, Gallery } from './styles';
+import { Container, SobreBox, Gallery,
+  One, Two, Three, Four, Five, Six, Seven, Eight
+} from './styles';
+
+import sq1 from './../../assets/images/sobre/sq.jpg';
+import sq2 from './../../assets/images/sobre/sq1.jpg';
+import sq3 from './../../assets/images/sobre/sq2.jpg';
+import sq4 from './../../assets/images/sobre/sq3.jpg';
+import sq5 from './../../assets/images/sobre/sq4.jpg';
+import sq6 from './../../assets/images/sobre/sq5.jpg';
+import sqb from './../../assets/images/sobre/sqb.jpg';
+import lg from './../../assets/images/sobre/lg.jpg';
 
 export default () => {
   return (
@@ -10,14 +21,11 @@ export default () => {
         <br/>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, perspiciatis totam quod eum vero nulla velit.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea quod saepe sed esse, consectetur architecto accusamus.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eius nobis ipsum corrupti non beatae odit?
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus in dolorem ab praesentium, quia nostrum vel?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </p>
         <br/>
         <br/>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quisquam corrupti quis quae adipisci sit voluptatibus!
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit non tempore, qui provident ducimus eaque nulla.
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores illum nostrum aspernatur laudantium iste, illo quos!
         </p>
@@ -27,31 +35,32 @@ export default () => {
           <b>Principios:</b>
           <br/>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit vitae illo id!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero laborum repudiandae ipsam.
+          Lorem, ipsum dolor sit.
         </p>
         <br/>
         <p>
           <b>Valores:</b>
           <br/>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit vitae illo id!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero laborum repudiandae ipsam.
+          Lorem, ipsum dolor sit amet.
         </p>
         <br/>
         <p>
           <b>Eticas:</b>
           <br/>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit vitae illo id!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero laborum repudiandae ipsam.
+          Lorem, ipsum dolor sit amet.
         </p>
       </SobreBox>
       <Gallery>
-        <div className="one"></div>
-        <div className="two"></div>
-        <div className="three"></div>
-        <div className="four"></div>
-        <div className="five"></div>
-        <div className="six"></div>
-        <div className="seven"></div>
+        <One bg={sq1} alt="food" className="one" />
+        <Two bg={sq2} alt="food" className="two" />
+        <Three bg={sq3} alt="food" className="three" />
+        <Four bg={sq4} alt="food" className="four" />
+        <Five bg={sq5} alt="food" className="eight" />
+        <Six bg={sq6} alt="food" className="five" />
+        <Seven bg={lg} alt="food" className="six" />
+        <Eight bg={sqb} alt="food" className="seven" />
       </Gallery>
     </Container>
   )
--- src/layout/Sobre/styles.js
+++ src/layout/Sobre/styles.js
@@ -1,30 +1,61 @@
 import styled from 'styled-components';
 
+import { dark_brown, light_gold } from './../../styles/variables';
+import { BackgroundImage } from './../../styles/mixins';
+import { SmoothTransition } from './../../styles/utils';
+
 export const Container = styled.section`
   padding: 1rem 5rem;
   display: grid;
-  grid-template-columns: 35% 65%;
+  grid-template-columns: 40% 60%;
+  background: ${dark_brown};
 `;
 
 export const SobreBox = styled.div`
+  padding: 5rem;
+
+  h1 {
+    color: ${light_gold};
+    font-size: 4rem;
+    text-align: center;
+    font-family: 'Akronim'
+  }
+
+  p {
+    color: #fff;
+    font-size: 1.5rem;
+    font-family: 'Dancing Script';
+    letter-spacing: 2px;
+
+    b {
+      margin: 1rem;
+      font-size: 2rem;
+      text-decoration: underline;
+    }
+  }
 `;
 
 export const Gallery = styled.div`
   padding: 1rem;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 1rem;
+  grid-gap: 1rem; 
   grid-template-areas: 
     "one two three"
     "four five six"
     "seven seven six"
-    "seven seven six";
+    "seven seven eight";
 
   > div {
-    box-shadow: 0 0 .3rem rgba(0,0,0, .5);
-    padding: 1rem;
+    box-shadow: 0 0 .3rem ${light_gold}; 
     border-radius: 1rem;
-  }
+    width: 100%; 
+    ${SmoothTransition};
+
+    &:hover {
+      transform: scale(1.07);
+    }
+  } 
 
   .one {
     grid-area: one;
@@ -53,4 +84,40 @@ export const Gallery = styled.div`
   .seven {
     grid-area: seven;
   }
+
+  .eight {
+    grid-area: eight;
+  }
+`;
+
+export const One = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Two = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Three = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Four = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Five = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Six = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Seven = styled.div`
+  ${props => BackgroundImage(props.bg)};
+`;
+
+export const Eight = styled.div`
+  ${props => BackgroundImage(props.bg)};
 `;
